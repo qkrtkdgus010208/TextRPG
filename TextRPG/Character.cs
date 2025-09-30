e3cc3a8e56682f4dcc43b4319cbd85c429b73b76
qkrtkdgus010208 <psh010208@naver.com>
2025-09-30 12:14:41 +0900

    feat: 캐릭터 스크립트 추가

---
+++ TextRPG/Character.cs
@@ -0,0 +1,73 @@
+﻿namespace TextRPG
+{
+    public enum JobType { Warrior, Mage, Archer }
+
+    internal class Character
+    {
+        public int Level { get; private set; }
+        public string Name { get; private set; }
+        public JobType Job { get; private set; }
+        public int Attack { get; private set; }
+        public int Armor { get; private set; }
+        public int Hp { get; private set; }
+        public int Gold { get; private set; }
+
+        public int BonusAttack { get; private set; }
+        public int BonusArmor { get; private set; }
+
+        public Character()
+        {
+            Level = 1;
+            Name = string.Empty;
+            Job = JobType.Warrior;
+            Attack = 10;
+            Armor = 5;
+            Hp = 100;
+            Gold = 1500;
+        }
+
+        public Character(JobType newJob, int newAttack, int newArmor, int newHp, int newGold)
+        {
+            Level = 1;
+            Name = string.Empty;
+            Job = newJob;
+            Attack = newAttack;
+            Armor = newArmor;
+            Hp = newHp;
+            Gold = newGold;
+        }
+
+        public void UpdateStats(List<Item> inventory)
+        {
+            // Bonus 능력치 초기화
+            BonusAttack = 0;
+            BonusArmor = 0;
+
+            // 인벤토리를 순회하며 장착된 아이템의 효과를 합산합니다.
+            foreach (var item in inventory)
+            {
+                if (item.IsEquipped)
+                {
+                    if (item.Type == ItemType.Weapon)
+                    {
+                        BonusAttack += item.EffectValue;
+                    }
+                    else if (item.Type == ItemType.Armor)
+                    {
+                        BonusArmor += item.EffectValue;
+                    }
+                }
+            }
+        }
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Lv. {Level}");
+            Console.WriteLine($"직업: {Job}");
+            Console.WriteLine($"공격력: {Attack + BonusAttack} {(BonusAttack != 0 ? $"(+{BonusAttack})" : "")}");
+            Console.WriteLine($"방어력: {Armor + BonusArmor} {(BonusArmor != 0 ? $"(+{BonusArmor})" : "")}");
+            Console.WriteLine($"체력: {Hp}");
+            Console.WriteLine($"Gold: {Gold} G");
+        }
+    }
+}
