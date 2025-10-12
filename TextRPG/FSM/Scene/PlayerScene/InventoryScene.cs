42b425ec5efda07d7252a0f85b92f1ec86614079
qkrtkdgus010208 <psh010208@naver.com>
2025-10-12 19:24:13 +0900

    test: 테스트용 아이템 추가 코드 삭제

--- TextRPG/FSM/Scene/PlayerScene/InventoryScene.cs
+++ TextRPG/FSM/Scene/PlayerScene/InventoryScene.cs
@@ -15,28 +15,6 @@ namespace TextRPG.FSM.Scene
         {
             Console.Title = "인벤토리";
             inventory = GameManager.Instance.Character.Inventory;
-
-            // 테스트용 아이템 추가
-            inventory.AddItem(new EquipItem("전사 무기1", "전사 무기1...", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0));
-            inventory.AddItem(new EquipItem("전사 무기2", "전사 무기2...", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Weapon, 0, 0, 20, 10, 0));
-            inventory.AddItem(new EquipItem("궁수 무기1", "궁수 무기1...", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0));
-            inventory.AddItem(new EquipItem("궁수 무기2", "궁수 무기2...", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Weapon, 0, 0, 20, 10, 0));
-            inventory.AddItem(new EquipItem("법사 무기1", "법사 무기1...", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Weapon, 0, 0, 10, 5, 0));
-            inventory.AddItem(new EquipItem("법사 무기2", "법사 무기2...", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Weapon, 0, 0, 20, 10, 0));
-
-            inventory.AddItem(new EquipItem("전사 방어구1", "전사 방어구1...", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Armor, 50, 10, 0, 0, 5, 5));
-            inventory.AddItem(new EquipItem("전사 방어구2", "전사 방어구2...", 1000, Enum.ItemType.Equip, Enum.JobType.Warrior, Enum.EquipSlot.Armor, 100, 20, 0, 0, 10, 10));
-            inventory.AddItem(new EquipItem("궁수 방어구1", "궁수 방어구1...", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Armor, 50, 10, 0, 0, 5, 5));
-            inventory.AddItem(new EquipItem("궁수 방어구2", "궁수 방어구2...", 1000, Enum.ItemType.Equip, Enum.JobType.Archer, Enum.EquipSlot.Armor, 100, 20, 0, 0, 10, 10));
-            inventory.AddItem(new EquipItem("법사 방어구1", "법사 방어구1...", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Armor, 50, 10, 0, 0, 5, 5));
-            inventory.AddItem(new EquipItem("법사 방어구2", "법사 방어구2...", 1000, Enum.ItemType.Equip, Enum.JobType.Mage, Enum.EquipSlot.Armor, 100, 20, 0, 0, 10, 10));
-
-            inventory.AddItem(new ConsumeItem("소비 아이템1", "Hp를 50 회복시켜줍니다.", 1000, 50, 0));
-            inventory.AddItem(new ConsumeItem("소비 아이템2", "Hp를 100 회복시켜줍니다.", 2000, 100, 0));
-            inventory.AddItem(new ConsumeItem("소비 아이템3", "Mp를 50 회복시켜줍니다.", 1000, 0, 50));
-            inventory.AddItem(new ConsumeItem("소비 아이템4", "Mp를 100 회복시켜줍니다.", 2000, 0, 100));
-            inventory.AddItem(new ConsumeItem("소비 아이템5", "Hp, Mp를 모두 50 회복시켜줍니다.", 2000, 50, 50));
-            inventory.AddItem(new ConsumeItem("소비 아이템6", "Hp, Mp를 모두 100 회복시켜줍니다.", 4000, 100, 100));
         }
 
         protected override void View()
