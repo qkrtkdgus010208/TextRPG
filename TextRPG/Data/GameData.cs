7f8364c3cfd7a65a47b3aa8fec97f0e9853a1596
qkrtkdgus010208 <psh010208@naver.com>
2025-10-13 20:03:29 +0900

    feat: GameData 추가

---
+++ TextRPG/Data/GameData.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TextRPG.Entity;
+
+namespace TextRPG.Data
+{
+    internal class GameData
+    {
+        public Character Character { get; set; }
+    }
+}
